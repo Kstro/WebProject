ac39f4fe2827df01887e9be81600d1a02aa6469e
Mario Castro <mecc_3@msn.com>
2014-10-27 10:56:59 -0600

    Versión inicial

---
+++ Templates/inicio.dwt.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Sistema SIOV</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+<link href="../css/frontend.css" rel="stylesheet" type="text/css" />
+<link href='http://fonts.googleapis.com/css?family=Istok+Web' rel='stylesheet' type='text/css'> <!-- link de google.fonts --> 
+
+</head>
+<body>
+<div class="container">
+  <div class="header"> 
+    <?php include('php/encabezado.php'); ?>
+    <?php include('php/menu.php'); ?>
+    <!-- end .header --></div>
+  
+  <div class="content"><!-- TemplateBeginEditable name="Contenido" -->Contenido<!-- TemplateEndEditable -->
+    <!-- end .content --></div>
+  <div class="footer1">
+    <?php include('php/pie_pagina.php'); ?>
+    <!-- end .footer --></div>
+  <div class="footer2">
+    
+    <!-- end .footer --></div>  
+    
+<!-- end .container --></div>
+</body>
+</html>
---
+++ php/menu.php
@@ -0,0 +1,66 @@
+<ul class="menu">
+	<li><a href="#"> Inicio </a>
+		<ul>
+		 	<li><a href="#"> Cerrar sesión </a></li>	 
+		</ul>
+    </li>
+	<li>
+	    <a href="#">Evaluación</a>
+		<ul> 
+			<li><a href="#"> Test Carácter de Griger </a></li>
+            <li><a href="#"> Test CHASIDE </a></li>	 
+            <li><a href="#"> Test Explora </a></li>
+            <li><a href="#"> Test de inteligencia OTIS </a></li>
+            <li><a href="#"> Test Inteligencias múltiples </a></li>
+            <li><a href="#"> Test Inventario de hábitos </a></li>
+            <li><a href="#"> Test Inventario pensamiento </a></li>
+            <li><a href="#"> Test La persona bajo la lluvia </a></li>
+            <li><a href="#"> Test Matrices de RAVEN </a></li>
+            <li><a href="#"> Test PMA </a></li>
+		</ul>
+	</li>
+	<li>
+	    <a href="#"> Expediente </a>
+		<ul>
+             		<li><a href="#"> Nuevo expediente </a></li>
+             		<li><a href="#"> Consultar expediente </a></li>	 
+		</ul>
+	 </li>
+	 <li>
+	    <a href="#"> Mantenimiento </a>
+		<ul>
+             		<li><a href="#"> Carreras </a></li>
+             		<li><a href="#"> Categoría test </a></li>
+             		<li><a href="#"> Departamento </a></li>
+             		<li><a href="#"> Instituciones </a></li>
+             		<li><a href="#"> Municipios </a></li>
+             		<li><a href="#"> Pregunta </a></li>
+             		<li><a href="#"> Roles usuario </a></li>
+             		<li><a href="#"> Test </a></li>
+             		<li><a href="#"> Tipo pregunta </a></li>
+             		<li><a href="#"> Usuarios </a></li>
+		</ul>
+	 </li> 
+	 <li>
+	     <a href="#"> Reportes </a>
+		<ul>
+             		<li><a href="#"> Estadísticos </a></li>	 
+             		<li><a href="#"> Expediente </a></li>
+             		<li><a href="#"> Test </a></li>
+		</ul>
+	 </li>
+	 <li>
+	      <a href="#"> Videojuego </a>
+		<ul>
+		 	<li><a href="#"> Iniciar </a></li>	 
+		</ul>
+	 </li>
+	 <li>
+	      <a href="#"> Ayuda </a>
+		<ul>
+             		<li><a href="#"> Manuales </a></li>
+             		<li><a href="#"> Videos </a></li>	 	 
+		</ul>
+	 </li>
+</ul>
+<div class="clearfloat"></div>
